2409cb4b89f88467454e86bbe3cdf5a97269ac18
hesam <hesam.backend@gmail.com>
2026-06-27 14:46:24 +0330

    fix theme selector issue for mobile device

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,24 @@
+window.onload = () => {
+  const theme = localStorage.getItem("theme")
+  const activeItem = document.querySelector(`#themeDropDown [data-theme="${theme}"]`)
+  activeItem.classList.add("active-theme")
+  const activeTheme = document.getElementById("activeTheme")
+  const themeDropDown = document.getElementById("themeDropDown")
+
+  activeTheme.addEventListener("click", function (e) {
+    e.stopPropagation()
+    themeDropDown.classList.toggle("opacity-0")
+  })
+
+  document.addEventListener("click", function () {
+      themeDropDown.classList.add("opacity-0")
+  })
+
+  themeDropDown.addEventListener("click", function (e) {
+      e.stopPropagation()
+  })
+}
+
 const menuToggle = () => {
     const menu = document.getElementById("sideMenu")
     const overlay = document.getElementById("overlay")
@@ -7,25 +28,27 @@ const menuToggle = () => {
 }
 
 const setTheme = (item) => {
-    const html = document.documentElement;
-    const theme = item.dataset.theme    
-    const activeItem = document.querySelector("#themeDropDown .active-theme")
+  const html = document.documentElement
+  const theme = item.dataset.theme    
+  const activeItem = document.querySelector("#themeDropDown .active-theme")
 
-    html.classList.remove("dark");
-    if (theme === "dark") {
-      html.classList.add("dark");
-    }
-  
-    if (theme === "system" && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      html.classList.add("dark");
-    }
-    localStorage.setItem("theme", theme)
-    activeItem.classList.remove("active-theme")
-    item.classList.add("active-theme")
-}
+  html.classList.remove("dark")
 
-window.onload = () => {
-    const theme = localStorage.getItem("theme")
-    const activeItem = document.querySelector(`#themeDropDown [data-theme="${theme}"]`)
-    activeItem.classList.add("active-theme")
+  if (theme === "dark") {
+      html.classList.add("dark")
+  }
+
+  if (
+      theme === "system" &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+      html.classList.add("dark")
+  }
+
+  localStorage.setItem("theme", theme)
+
+  activeItem.classList.remove("active-theme")
+  item.classList.add("active-theme")
+
+  themeDropDown.classList.add("opacity-0")
 }
